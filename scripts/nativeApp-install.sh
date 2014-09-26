9f6f7565a695a60e3ca3700d1e8c0a414553e676
Amr Aboelela <amraboelela@gmail.com>
2014-09-26 16:56:34 -0700

    cleanup

--- scripts/nativeApp-install.sh
+++ scripts/nativeApp-install.sh
@@ -11,7 +11,7 @@ if [ ${TARGET} = "All" ]; then
     TARGET=All
 else
     source ${MYOS_PATH}/android/sdk/scripts/build.sh
-cd ${APPLICATION_PATH}
+    cd ${APPLICATION_PATH}
     export TARGET=${TARGET}
     source ${MYOS_PATH}/android/sdk/scripts/common-make.sh
 fi
